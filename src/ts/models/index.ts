6ab8230ebe7d844c5a332ee9263ed04b0880f081
EvgeniaM6 <evgminukova6@gmail.com>
2023-01-13 00:42:17 +0200

    feat: add car and request models

---
+++ src/ts/models/index.ts
@@ -0,0 +1,4 @@
+import { ICar } from './car.model';
+import { EMethod } from './request.model';
+
+export { EMethod, ICar };
---
+++ src/ts/models/request.model.ts
@@ -0,0 +1,5 @@
+export enum EMethod {
+  Get = 'GET',
+  Post = 'POST',
+  Put = 'PUT',
+}
