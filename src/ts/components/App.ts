b3e9b981dc587e2f67f88ff31f76dd043d393218
EvgeniaM6 <evgminukova6@gmail.com>
2023-01-15 15:16:47 +0200

    feat: start game module

--- src/ts/components/App.ts
+++ src/ts/components/App.ts
@@ -1,9 +1,12 @@
+import DataBase from './DataBase';
 import Game from './Game';
 
 export default class App {
+  dataBase: DataBase;
   game: Game;
 
   constructor() {
+    this.dataBase = new DataBase();
     this.game = new Game();
   }
 }
